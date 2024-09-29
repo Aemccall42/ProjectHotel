be514544281be91855a2aaf86cc1b4e5b6fbbec7
Abigail McCall <aemccall42@tntech.edu>
2024-09-29 12:07:52 -0500

    Co-authored-by: DeezyDeeDEE <DeezyDeeDEE@users.noreply.github.com> Co-authored-by: kjoshi42 <kjoshi42@users.noreply.github.com>

--- HotelChain.cpp
+++ HotelChain.cpp
@@ -69,17 +69,6 @@ void HotelChain::displayRooms() const{
 
 
 */
-//needed functions(?): Contructor, Destructor, Display, 
-
-/*class HotelChain
-//Requirements: be able to read items from a file, be able to print the contents to the screen.
-{
-private:
-//Variables: number of hotels, ...
-string hotelName;    
-public:
-   
    
 
 
-};*/
